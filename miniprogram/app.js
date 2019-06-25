2e7a1d1e87ed15ffdc5204cd7481c5e53e945f78
kwkarlwang <kawang@ucsd.edu>
2019-06-24 18:02:54 -0700

    Update app.js

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,4 +1,4 @@
-//app.js
+ //app.js
 App({
   globalData: {
     appid: 'wxd6397161949fd434',
@@ -37,7 +37,7 @@ App({
             }
             console.log(res.data)
             that.globalData.AttendEvent = res.data[0].AttendEvent
-            that.globalData.SponsorEvent = res.data[0].SponserEvent
+            that.globalData.SponsorEvent = res.data[0].SponsorEvent
             console.log(that.globalData)
             wx.getSetting({
               success: function(res) {
